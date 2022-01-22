67305d14c61d1b02fd3b794c25e2f3d9dfc8f0d9
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-01-22 14:00:55 -0500

    Agregar una plantilla personalizada.
    Se agrega el comentario en el nuevo template con template name.

---
+++ wp-content/themes/platzigifts/template-instutucional.php
@@ -0,0 +1,20 @@
+<?php
+//Template Name: Página Institucional
+get_header();
+?>
+
+<main class='container'>
+	<?php if(have_posts()){
+			while(have_posts()){
+				the_post(); ?>
+				<h1 class='my-3'><?= the_title(); ?></h1>
+
+				<?= the_content(); ?>
+
+			<?php
+			}
+	}
+	?>
+</main>
+
+<?= get_footer(); ?>
