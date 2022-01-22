3c69d350528529274f364fda33fd7e37e08c75cc
Eddy Huaylla <ehuaylla@attachmedia.com>
2022-01-22 14:52:38 -0500

    Agregar shortcode en detalle-producto del formulario generado en contact form 7

--- wp-content/themes/platzigifts/single-producto.php
+++ wp-content/themes/platzigifts/single-producto.php
@@ -12,10 +12,13 @@
 
 				<h1 class='my-5'><?= the_title() ?></h1>
 				<div class="row">
-					<div class="col-6">
+					<div class="col-md-6 col-12">
 						<?= the_post_thumbnail('large'); ?>
 					</div>
-					<div class="col-6">
+					<div class="col-md-6 col-12">
+						<?= do_shortcode('[contact-form-7 id="33" title="Formulario de Contacto"]'); ?>
+					</div>
+					<div class="col-12">
 						<?= the_content(); ?>
 					</div>
 				</div>
